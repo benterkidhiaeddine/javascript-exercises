281772782f5cba52bb69a57964531c92a6d1235f
benterkidhiaeddine <benterki.dhiaeddine@gmail.com>
2023-01-02 20:33:58 +0100

    Solved

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,15 @@
-const leapYears = function() {
-
+const leapYears = function(year) {
+    //a leap year is a year divisible by 4
+    if (year % 4 == 0){
+        if(year %100 == 0){
+            if(year % 400 == 0){
+                return true;
+            }
+            return false;
+        }
+        return true;
+    }
+    return false;
 };
 
 // Do not edit below this line
