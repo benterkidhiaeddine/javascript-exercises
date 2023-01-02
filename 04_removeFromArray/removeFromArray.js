5aa7886e0bca1c8cd59c4aae6cdeaa876cbc2457
benterkidhiaeddine <benterki.dhiaeddine@gmail.com>
2023-01-02 19:31:33 +0100

    Solved

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,20 @@
-const removeFromArray = function() {
+const removeFromArray = function(list,...args) {
+    //remember elements to be removed in a dict
+    let dict=new Map();
+    result=[];
+    l = args.length;
+    for (let i= 0 ;i<l;i++)
+    {
+        dict.set(args[i],i);
+    }
+    for (el of list){
+        //if the element of the list was not used to create the hashmap then it should not be added to the result list
+        if(dict.get(el)==undefined){
+            result.push(el);
+        }
 
+    }
+    return result;
 };
 
 // Do not edit below this line
