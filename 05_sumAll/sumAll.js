05b66c94dc5b667ce48f241330ad38c37bd3e941
benterkidhiaeddine <benterki.dhiaeddine@gmail.com>
2023-01-02 20:25:34 +0100

    Solved

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,20 @@
-const sumAll = function() {
-
+const sumAll = function(int1,int2) {
+    if(typeof(int1) != 'number'||typeof(int2) != "number"){
+        return "ERROR";
+    }
+    if(int1< 0 || int2< 0)
+    {
+        return "ERROR";
+    }
+    let big=int1<int2?int2:int1;
+    let small=int1>int2?int2:int1;
+    let sum=0;
+    while (small<=big)
+    {
+        sum+=small;
+        small++;
+    }
+    return sum;
 };
 
 // Do not edit below this line
