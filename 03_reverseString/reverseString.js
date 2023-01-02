b94c7caff962238fcf5e133bb7b5b0f393428a74
benterkidhiaeddine <benterki.dhiaeddine@gmail.com>
2023-01-02 18:29:29 +0100

    Solved

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,10 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    let result ="";
+    let l=string.length;
+    for (l;l>0;l--){
+        result += string[l-1];
+    }
+    return result;
 };
 
 // Do not edit below this line
