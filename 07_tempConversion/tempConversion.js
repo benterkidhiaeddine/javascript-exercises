77b9534c5ac6f443cbf27f19f37f03304b74c463
benterkidhiaeddine <benterki.dhiaeddine@gmail.com>
2023-01-02 21:46:40 +0100

    Solved

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,11 +1,22 @@
-const convertToCelsius = function() {
+const roundToOneDecimal=function(number){
+  result = (Math.round(number * 10)/10);
+  return result;
+
+}
+const convertToCelsius = function(F) {
+  //round the number to 1 decimal
+  result = roundToOneDecimal((F - 32) * 5/9 );
+  return result;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(C) {
+  //round the number to 1 decimal
+  return roundToOneDecimal((C * 9/5) + 32);
 };
 
 // Do not edit below this line
 module.exports = {
   convertToCelsius,
-  convertToFahrenheit
+  convertToFahrenheit,
+  roundToOneDecimal
 };
